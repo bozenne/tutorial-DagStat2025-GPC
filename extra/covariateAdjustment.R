7b4271d06f8c3d8269cd353dca97ea29296008fc
bozenne <brice.mh.ozenne@gmail.com>
2025-04-21 15:22:20 +0200

    update standardization with build-in approach

--- extra/covariateAdjustment.R
+++ extra/covariateAdjustment.R
@@ -3,9 +3,9 @@
 ## Author: Brice Ozenne
 ## Created: mar 19 2025 (18:42) 
 ## Version: 
-## Last-Updated: mar 19 2025 (18:42) 
+## Last-Updated: Apr 21 2025 (15:22) 
 ##           By: Brice Ozenne
-##     Update #: 1
+##     Update #: 3
 ##----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -62,6 +62,62 @@ confint(s.BR, strata = TRUE)
 ## toxicity.M    0.13014476 0.05847593  0.014300729 0.2425413    0 0.027778835
 ## toxicity.F    0.08636752 0.06352251 -0.038834727 0.2088999    0 0.176098720
 
+## ** standardization
+
+## *** build-in
+std.BR <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + tte(OS, statusOS, threshold = 2) + cont(toxicity, operator = "<0") + strata(sex),
+                    pool.strata = "standardization", data = prodige)
+summary(std.BR, digit = c(2,3,2))
+## endpoint threshold strata total(%) favorable(%) unfavorable(%) neutral(%) uninf(%)  delta Delta CI [2.5% ; 97.5%] p.value    
+##       OS         6 global   100.00        28.16          15.71      56.04     0.09  0.126 0.126     [0.054;0.197] 0.00067 ***
+##                         M    24.47         6.88           3.42      14.14     0.04  0.142                                    
+##                       F.M    27.31         7.42           4.90      14.97     0.02  0.092                                    
+##                       M.F    22.79         6.66           3.00      13.11     0.02  0.160                                    
+##                         F    25.43         7.20           4.39      13.83     0.01  0.111                                    
+## toxicity         2 global    56.13        10.27          13.01      32.85     0.00 -0.028 0.098     [0.019;0.176] 0.01578   *
+##                         M    14.18         2.76           3.14       8.29     0.00 -0.016                                    
+##                       F.M    14.99         2.79           3.25       8.94     0.00 -0.017                                    
+##                       M.F    13.13         2.34           3.24       7.55     0.00 -0.040                                    
+##                         F    13.84         2.38           3.38       8.07     0.00 -0.039                                    
+##       OS         2 global    32.85         5.08           5.02      22.71     0.04  0.001 0.099     [0.016;0.181] 0.01984   *
+##                         M     8.29         1.11           1.23       5.92     0.02 -0.005                                    
+##                       F.M     8.94         1.37           1.39       6.19     0.00 -0.001                                    
+##                       M.F     7.55         1.22           1.12       5.18     0.02  0.004                                    
+##                         F     8.07         1.38           1.28       5.41     0.00  0.004                                    
+## toxicity           global    22.75         5.73           4.69      12.33     0.00  0.010 0.109     [0.022;0.194] 0.01389   *
+##                         M     5.95         1.41           1.19       3.34     0.00  0.009                                    
+##                       F.M     6.19         1.64           1.24       3.30     0.00  0.015                                    
+##                       M.F     5.20         1.24           1.10       2.86     0.00  0.006                                    
+##                         F     5.41         1.44           1.15       2.82     0.00  0.011                                    
+
+coef(s.BR) ## marginal
+##      OS_t6 toxicity_t2       OS_t2    toxicity 
+## 0.12592885  0.09833291  0.09802122  0.10802346 
+coef(std.BR)  ## stratified followed by standardization
+##      OS_t6 toxicity_t2       OS_t2    toxicity 
+## 0.12604879  0.09807780  0.09885171  0.10900857 
+
+## *** manual
+## WARNING: does not match the build-in approach due to a different censoring model (survival probabilities only treatment dependent but not sex dependent)
+##          uncertainty not properly computed as the software mis-understand the number of independent observations (taken to be the sum of the weights instead of the number of lines in the dataset)
+tableN <- addmargins(table(treatment = prodige$treatment,sex = prodige$sex))
+df.tableN <- as.data.frame(tableN[1:2,1:2])
+rownames(df.tableN) <- paste(df.tableN$treatment,df.tableN$sex, sep = ":")
+
+prodige$weight <- sqrt(tableN["C","Sum"]*tableN["T","Sum"])/tableN["Sum","Sum"]*tableN["Sum",as.character(prodige$sex)]/df.tableN[paste(prodige$treatment,prodige$sex,sep=":"),"Freq"]
+std2.BR <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + tte(OS, statusOS, threshold = 2) + cont(toxicity, operator = "<0"),
+                    data = prodige, weightObs = prodige$weight, trace = FALSE)
+coef(std2.BR, endpoint = "toxicity") 
+## [1] 0.1119014
+
+## to be compared to
+coef(BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + tte(OS, statusOS, threshold = 2) + cont(toxicity, operator = "<0") + strata(sex),
+               pool.strata = "standardization", data = prodige, model.tte =  prodlim(Hist(OS, statusOS)~treatment, data = prodige), trace = FALSE))
+##     OS_t6 toxicity_t2       OS_t2    toxicity 
+## 0.1282043   0.1008404   0.1016326   0.1119014 
+
+## another approach would be to fit a GPC for each combination of strata among treatment groups: (active.control)=(M.M, F.M, M.F, F.F) and pool with appropriate weights
+
 ## ** Non-parametric adjustment
 marginal.BR <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + tte(OS, statusOS, threshold = 2) + cont(toxicity, operator = "<0"),
                          data = prodige, trace = FALSE)
@@ -73,54 +129,6 @@ Vmat2 <- crossprod(cbind(getIid(marginal.BR, endpoint = "toxicity"),getIid(strat
 coef(marginal.BR, endpoint = "toxicity") -  Vmat2[1,-1,drop=FALSE] %*% solve(Vmat2[-1,-1,drop=FALSE]) %*% coef(strata.BR) ## 0.1122007
 ## variance
 Vmat2[1,1] - Vmat2[1,-1,drop=FALSE] %*% solve(Vmat2[-1,-1,drop=FALSE]) %*% Vmat2[-1,1,drop=FALSE] ## 0.001847724
-
-## ** Alternative: standardization
-tableN <- addmargins(table(treatment = prodige$treatment,sex = prodige$sex))
-df.tableN <- as.data.frame(tableN[1:2,1:2])
-rownames(df.tableN) <- paste(df.tableN$treatment,df.tableN$sex, sep = ":")
-
-prodige$weight <- sqrt(tableN["C","Sum"]*tableN["T","Sum"])/tableN["Sum","Sum"]*tableN["Sum",as.character(prodige$sex)]/df.tableN[paste(prodige$treatment,prodige$sex,sep=":"),"Freq"]
-std.BR <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + tte(OS, statusOS, threshold = 2) + cont(toxicity, operator = "<0"),
-                    data = prodige, weightObs = prodige$weight, trace = FALSE)
-confint(std.BR, endpoint = "toxicity") ## WARNING 1: uncertainty ignore variability of the weights, i.e., assume known covariate distribution
-                                       ## WARNING 2: survival probabilities only treatment dependent but not sex dependent
-##           estimate        se   lower.ci  upper.ci null   p.value
-## toxicity 0.1119014 0.0431152 0.02678982 0.1954023    0 0.0100622
-
-#### EXTRA: equivalence between explicit standardization and standardization using weights            ####
-####        require same survival model - illustrated only with survival only depending on treatment  ####
-
-## approach 1 with weights
-e.ref <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2),
-                   model.tte = prodlim(Hist(OS,statusOS)~treatment, data = prodige), data = prodige, trace = FALSE)
-
-confint(e.ref, endpoint = "toxicity_t2")
-##              estimate         se   lower.ci  upper.ci null   p.value
-## toxicity_t2 0.1000839 0.03955895 0.02209791 0.1768593    0 0.0119687
-
-## approach 2 explicit standardization (omitting the argument model.tte is prefered as survival probabilities would be treatment and sex dependent)
-prodige$sex2 <- ifelse(prodige$treatment=="T",prodige$sex=="M",prodige$sex=="F")
-e.strata1 <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + strata(sex),
-                       model.tte = prodlim(Hist(OS,statusOS)~treatment, data = prodige), data = prodige, trace = FALSE)
-e.strata2 <- BuyseTest(treatment ~ tte(OS, statusOS, threshold = 6) + cont(toxicity, operator = "<0", threshold = 2) + strata(sex2),
-                       model.tte = prodlim(Hist(OS,statusOS)~treatment, data = prodige), data = prodige, trace = FALSE)
-
-eDelta.strata1 <- coef(e.strata1, strata = TRUE, endpoint = "toxicity_t2")
-eDelta.strata2 <- coef(e.strata2, strata = TRUE, endpoint = "toxicity_t2")
-
-eTable.std <- rbind(data.frame(sex.C = names(eDelta.strata1),
-                               pi.C = tableN["C",names(eDelta.strata1)]/tableN["C","Sum"],
-                               sex.T = names(eDelta.strata1),
-                               pi.T = tableN["T",names(eDelta.strata1)]/tableN["T","Sum"],
-                               Delta = eDelta.strata1),
-                    data.frame(sex.C = c("M","F"),
-                               pi.C = tableN["C",c("M","F")]/tableN["C","Sum"],
-                               sex.T = c("F","M"),
-                               pi.T = tableN["T",c("F","M")]/tableN["T","Sum"],
-                               Delta = eDelta.strata2)
-                    )
-sum(eTable.std$pi.C*eTable.std$pi.T*eTable.std$Delta)
-## [1] 0.1000839
 
 
 
